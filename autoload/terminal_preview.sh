4b490f41fcc0522586571f539f6400cff99b0fce
Huawen Yu <hyu@fortinet.com>
2024-01-14 21:29:03 -0800

    update

--- autoload/terminal_preview.sh
+++ autoload/terminal_preview.sh
@@ -15,6 +15,78 @@ fileout="/tmp/vim_a.out"
 #echo "===vim-floaterm-repl:terminal_preview.sh===="
 #echo "---------------------"
 
+# @require please prepare the executable {fileout}
+# @arg1 the interpreter if required
+function try_run_me() {
+    ######################################################
+    ###### For example, put our testcase as comment like this
+    ######################################################
+    #/*
+    #https://linuxhint.com/using_mmap_function_linux/
+    #mmap used to Writing file
+
+    #size-of
+    #	 file   map  write unmap
+    #	 1024  2048  2048  2048
+
+    #>>> {fileout}  1024  2048  2048  2048
+    #>>> echo Normal
+
+    #>>> {fileout}  1024  2048  2048  3048
+    #>>> echo OK: unmap more size
+
+    #>>> {fileout}  1024  2048  2048  5048
+    #>>> echo ERROR: unmap > 4K page (Segmentation-fault)
+
+    #>>> {fileout}  1024  2048  4096  2048
+    #>>> echo OK: write < 4K-page
+
+    #>>> {fileout}  1024  2048  4097  2048
+    #>>> echo ERROR: write > 4K page (Bus error)
+
+    #>>> size {fileout}
+    #>>> ls -l {file}
+    #*/
+    interpreter=""
+    if [[ $# -ge 1 ]]; then
+        interpreter=$@
+    fi
+
+    # If code-fence provide test-data, use it, otherwise run directly
+    # echo "LIBRARY_TRGT_CANV,CANV_MATCH<anything>"|awk -F "_TRGT_" '{print $NF}'
+    # NF=2: testcase
+    # NF=3: command
+    if ! grep -q "^>>> " $filepath; then
+        $interpreter   $fileout $params
+    fi
+
+    if grep -q "^>>>" $filepath; then
+        awk -v fileout=$fileout -v filepath=$filepath -v interpreter=$interpreter '
+              BEGIN {
+                  FS=">>>"
+              }
+              /^>>>/ {
+                  gsub("{file}", filepath, $NF)
+                  gsub("{fileout}", fileout, $NF)
+
+                  is_echo = match($NF, /([ \t]*)echo([ \t]*)(.*)$/, arr)
+                  if (is_echo != 0) {
+                      #print "#" $NF
+                      print "#" arr[3]
+                  }
+                  else {
+                      system("echo;")
+                      system("echo $ " $NF ";" interpreter " " $NF)
+                  }
+              }' $filepath
+        ## if has command, do it
+        #if grep -q "^<<<" $filepath; then
+        #    awk -v fileout=$fileout -v filepath=$filepath 'BEGIN {FS="<<<"}/^>>>/{gsub("{file}", filepath, $NF); gsub("{fileout}", fileout, $NF); system("echo " $NF ";" $NF)}' $filepath
+        #fi
+    fi
+}
+
+
 if ! command -v boxes &> /dev/null; then
     echo "Start $filetype"
 else
@@ -28,11 +100,15 @@ case $filetype in
     ;;
 
   bash | sh)
-     bash $filepath $params
+      #bash $filepath $params
+      cp $filepath $fileout
+      try_run_me bash
     ;;
 
   go )
-      go run $filepath $params
+      #go run $filepath $params
+      cp $filepath $fileout
+      try_run_me go run
     ;;
 
   rust )
@@ -57,73 +133,15 @@ case $filetype in
       gcc -pthread -lrt -g -O0 -finstrument-functions -fms-extensions -o $fileout $filepath
       #gcc -pthread -std=c11 -lrt -g -O0 -finstrument-functions -fms-extensions -o $fileout $filepath
 
-    ######################################################
-    ###### For example, put our testcase as comment like this
-    ######################################################
-	#/*
-	#https://linuxhint.com/using_mmap_function_linux/
-	#mmap used to Writing file
-
-	#size-of
-	#	 file   map  write unmap
-	#	 1024  2048  2048  2048
-
-	#>>> {fileout}  1024  2048  2048  2048
-	#>>> echo Normal
-
-	#>>> {fileout}  1024  2048  2048  3048
-	#>>> echo OK: unmap more size
-
-	#>>> {fileout}  1024  2048  2048  5048
-	#>>> echo ERROR: unmap > 4K page (Segmentation-fault)
-
-	#>>> {fileout}  1024  2048  4096  2048
-	#>>> echo OK: write < 4K-page
-
-	#>>> {fileout}  1024  2048  4097  2048
-	#>>> echo ERROR: write > 4K page (Bus error)
-
-	#>>> size {fileout}
-	#>>> ls -l {file}
-	#*/
-
-      # If code-fence provide test-data, use it, otherwise run directly
-      # echo "LIBRARY_TRGT_CANV,CANV_MATCH<anything>"|awk -F "_TRGT_" '{print $NF}'
-      # NF=2: testcase
-      # NF=3: command
-      if ! grep -q "^>>> " $filepath; then
-          $fileout $params
-      fi
-
-      if grep -q "^>>>" $filepath; then
-          awk -v fileout=$fileout -v filepath=$filepath '
-                BEGIN {
-                    FS=">>>"
-                }
-                /^>>>/ {
-                    gsub("{file}", filepath, $NF)
-                    gsub("{fileout}", fileout, $NF)
-
-                    is_echo = match($NF, /([ \t]*)echo([ \t]*)(.*)$/, arr)
-                    if (is_echo != 0) {
-                        #print "#" $NF
-                        print "#" arr[3]
-                    }
-                    else {
-                        system("echo;")
-                        system("echo $ " $NF ";" $NF)
-                    }
-                }' $filepath
-          ## if has command, do it
-          #if grep -q "^<<<" $filepath; then
-          #    awk -v fileout=$fileout -v filepath=$filepath 'BEGIN {FS="<<<"}/^>>>/{gsub("{file}", filepath, $NF); gsub("{fileout}", fileout, $NF); system("echo " $NF ";" $NF)}' $filepath
-          #fi
-      fi
+      #$fileout $params
+      try_run_me
     ;;
 
   cpp | cxx | CPP | CXX)
       g++ -pthread -lrt -g -O0 -finstrument-functions -fms-extensions -o $fileout $filepath
-      $fileout $params
+
+      #$fileout $params
+      try_run_me
     ;;
 
   *)
